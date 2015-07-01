04e10099257238768d3c4863b013f462362c7ffb
klemensl <none@none>
2015-07-01 09:21:29 +0200

    refactoring and added "GetLabels" for different actions

--- __init__.py
+++ __init__.py
@@ -11,20 +11,24 @@ eg.RegisterPlugin(
 )
 
 class HMXMLAPI(eg.PluginBase):
+    HM_XMLAPI_URL= "/config/xmlapi/"
+    HM_XMLAPI_STATE = "state.cgi?datapoint_id={0}"
+    HM_XMLAPI_STATECHANGE = "statechange.cgi?ise_id={0}&new_value={1}"
 
     def __init__(self):
         self.AddAction(sysvarlist)
         self.AddAction(statelist)
-        self.AddAction(turnonoroff)
-        self.AddAction(frompayload)
+        self.AddAction(turnOnOrOff)
+        self.AddAction(setValue)
+        self.AddAction(setValueFromPayload)
+        self.AddAction(callCGIfromPayload)
         self.AddAction(turnOnFromPayload)
         self.AddAction(turnOffFromPayload)
-        self.AddAction(setValue)
-		
+        
     def __start__(self, protocol, host, port):
         self.host = host
         print "Homematic XML API Plugin is started with parameter: " + self.host
-		
+        
     def Configure(self, protocol="HTTP", host="", port=80):
         panel = eg.ConfigPanel(self)
         protocolCtrl = panel.TextCtrl(protocol)
@@ -42,7 +46,7 @@ class HMXMLAPI(eg.PluginBase):
             panel.SetResult(
                 protocolCtrl.GetValue(),
                 hostCtrl.GetValue(),
-                portCtrl.GetValue(),
+                portCtrl.GetValue()
             )
 
     def SendRequest(self, request):
@@ -56,103 +60,133 @@ class HMXMLAPI(eg.PluginBase):
         #print response.status, response.reason, dataBody
         eg.globals.ccu2xmlapiresponse = dataBody
         return dataBody
-			
+
+    def changeState(self, ise_id, new_value):
+        self.SendRequest(self.HM_XMLAPI_URL + self.HM_XMLAPI_STATECHANGE.format(ise_id, new_value).lower())
+        dataBody = self.SendRequest(self.HM_XMLAPI_URL + self.HM_XMLAPI_STATE.format(ise_id).lower())
+        datapoint = ET.fromstring(dataBody).find('datapoint')
+        if datapoint is not None:
+            datapointValue = datapoint.get('value')
+        else:
+            datapointValue = "unknown"
+        eg.globals.ccu2statechangedto = datapointValue; 
+        print "ise_id {0} changed to {1}".format(ise_id, datapointValue)
+            
 
 class sysvarlist(eg.ActionBase):
-    name = "CCU2 Call: SysVarList"
+    name = "CCU2 Call: sysVarList.cgi"
 
     def __call__(self):
         print "Action 'SysVarList'"
-        data = self.plugin.SendRequest("/config/xmlapi/sysvarlist.cgi")
+        data = self.plugin.SendRequest(self.plugin.HM_XMLAPI_URL + "sysvarlist.cgi")
         eg.globals.ccu2statechangedto = ""
-			
+            
 
 class statelist(eg.ActionBase):
-    name = "CCU2 Call: StateList"
+    name = "CCU2 Call: stateList.cgi"
 
     def __call__(self):
         print "Action 'StateList'"
-        data = self.plugin.SendRequest("/config/xmlapi/statelist.cgi")
+        self.plugin.SendRequest(self.plugin.HM_XMLAPI_URL + "statelist.cgi")
         eg.globals.ccu2statechangedto = ""
-			
-class turnonoroff(eg.ActionBase):
-    name = "CCU2 Call: Turn Channel (ise_id) ON or OFF (new_value)"
 
+            
+class turnOnOrOff(eg.ActionBase):
     def __call__(self, ise_id, new_value):
         print "Action 'Statechange' - ID: {0}={1}".format(ise_id, new_value)
-        dataBody = self.plugin.SendRequest("/config/xmlapi/statechange.cgi?ise_id={0}&new_value={1}".format(ise_id, new_value).lower())
-        dataBody = self.plugin.SendRequest("/config/xmlapi/state.cgi?datapoint_id={0}".format(ise_id).lower())
-        eg.globals.ccu2statechangedto = ET.fromstring(dataBody).find('datapoint').get('value')
+        self.plugin.changeState(ise_id, new_value)
+
+    def GetLabel(self, ise_id, new_value):
+        return "Change state of ise_id {0} to {1}".format(ise_id, new_value) 
 
     def Configure(self, ise_id="", new_value=True):
         panel = eg.ConfigPanel(self)
         ise_idCtrl = panel.TextCtrl(ise_id)
         new_valueCtrl = panel.CheckBox(new_value)
-        panel.AddLine(panel.StaticText("Ise ID:"), ise_idCtrl)
+        panel.AddLine(panel.StaticText("ise_id:"), ise_idCtrl)
         panel.AddLine(panel.StaticText("ON/OFF:"), new_valueCtrl)
         while panel.Affirmed():
             panel.SetResult(
                 ise_idCtrl.GetValue(),
-                new_valueCtrl.GetValue(),
+                new_valueCtrl.GetValue()
             )
-			
-class setValue(eg.ActionBase):
-    name = "CCU2 Call: Set Channel (ise_id) value (new_value)"
 
+            
+class setValue(eg.ActionBase):
     def __call__(self, ise_id, new_value):
         print "Action 'Statechange' - ID: {0}={1}".format(ise_id, new_value)
-        dataBody = self.plugin.SendRequest("/config/xmlapi/statechange.cgi?ise_id={0}&new_value={1}".format(ise_id, new_value).lower())
-        dataBody = self.plugin.SendRequest("/config/xmlapi/state.cgi?datapoint_id={0}".format(ise_id).lower())
-        eg.globals.ccu2statechangedto = ET.fromstring(dataBody).find('datapoint').get('value')
+        self.plugin.changeState(ise_id, new_value)
+
+    def GetLabel(self, ise_id, new_value):
+        return "Set the value of ise_id {0} to {1}".format(ise_id, new_value) 
 
     def Configure(self, ise_id="", new_value=""):
         panel = eg.ConfigPanel(self)
         ise_idCtrl = panel.TextCtrl(ise_id)
         new_valueCtrl = panel.TextCtrl(new_value)
-        panel.AddLine(panel.StaticText("Ise ID:"), ise_idCtrl)
+        panel.AddLine(panel.StaticText("ise_id:"), ise_idCtrl)
         panel.AddLine(panel.StaticText("Value:"), new_valueCtrl)
         while panel.Affirmed():
             panel.SetResult(
                 ise_idCtrl.GetValue(),
-                new_valueCtrl.GetValue(),
+                new_valueCtrl.GetValue()
             )
-			
-class frompayload(eg.ActionBase):
+
+            
+class setValueFromPayload(eg.ActionBase):
+    def __call__(self, ise_id):
+        if not eg.event.payload:
+             print "No payload set - skipping..."
+             return False
+        new_value = eg.event.payload[0]
+        print "Action 'Statechange' - ID: {0}={1}".format(ise_id, new_value)
+        self.plugin.changeState(ise_id, new_value)
+
+    def GetLabel(self, ise_id, new_value):
+        return "Set the value of ise_id {0} to 'eg.event.payload[0]'".format(ise_id) 
+
+    def Configure(self, ise_id=""):
+        panel = eg.ConfigPanel(self)
+        ise_idCtrl = panel.TextCtrl(ise_id)
+        panel.AddLine(panel.StaticText("ise_id:"), ise_idCtrl)
+        while panel.Affirmed():
+            panel.SetResult(
+                ise_idCtrl.GetValue()
+            )
+
+            
+class callCGIfromPayload(eg.ActionBase):
     name = "CCU2 Call: From Event Payload"
-    description = "Call the CCU2 XML API with the Payload of the current event -> http://IP/config/xmlapi/EventPayload.cgi"
-	
+    description = "Call the CCU2 XML API .CGI with the Payload of the current event -> http://IP/config/xmlapi/eg.event.payload[0].cgi"
+    
     def __call__(self):
         if not eg.event.payload:
              print "No payload set - skipping..."
              return False
         print "Action 'From Payload' - Payload: " + eg.event.payload[0]
-        data = self.plugin.SendRequest("/config/xmlapi/" + eg.event.payload[0] + ".cgi")
-			
+        self.plugin.SendRequest(self.plugin.HM_XMLAPI_URL + eg.event.payload[0] + ".cgi")
+
 class turnOnFromPayload(eg.ActionBase):
-    name = "CCU2 Call: Turn ON from Event Payload"
+    name = "CCU2 Call: Turn ise_id 'eg.event.payload[0]' ON"
     description = ""
-	
+    
     def __call__(self):
         if not eg.event.payload:
              print "No payload set - skipping..."
              return False
         ise_id = eg.event.payload[0]
         print "Action 'Turn ON from Payload' - Payload: " + ise_id
-        dataBody = self.plugin.SendRequest("/config/xmlapi/statechange.cgi?ise_id={0}&new_value=true".format(ise_id).lower())
-        dataBody = self.plugin.SendRequest("/config/xmlapi/state.cgi?datapoint_id={0}".format(ise_id).lower())
-        eg.globals.ccu2statechangedto = ET.fromstring(dataBody).find('datapoint').get('value')
+        self.plugin.changeState(ise_id, "true")
 
-			
+            
 class turnOffFromPayload(eg.ActionBase):
-    name = "CCU2 Call: Turn OFF from Event Payload"
+    name = "CCU2 Call: Turn ise_id 'eg.event.payload[0]' OFF"
     description = ""
-	
+    
     def __call__(self):
         if not eg.event.payload:
              print "No payload set - skipping..."
              return False
         ise_id = eg.event.payload[0]
         print "Action 'Turn OFF from Payload' - Payload: " + ise_id
-        dataBody = self.plugin.SendRequest("/config/xmlapi/statechange.cgi?ise_id={0}&new_value=false".format(ise_id).lower())
-        dataBody = self.plugin.SendRequest("/config/xmlapi/state.cgi?datapoint_id={0}".format(ise_id).lower())
-        eg.globals.ccu2statechangedto = ET.fromstring(dataBody).find('datapoint').get('value')
+        self.plugin.changeState(ise_id, "false")
